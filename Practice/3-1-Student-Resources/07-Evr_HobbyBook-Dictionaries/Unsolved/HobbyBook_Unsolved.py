bfa0ff3a8c4c6649a26d80eac760d239d2753f48
Marco <marco_mireles@outlook.com>
2022-12-07 23:24:42 -0600

    practice

--- Practice/3-1-Student-Resources/07-Evr_HobbyBook-Dictionaries/Unsolved/HobbyBook_Unsolved.py
+++ Practice/3-1-Student-Resources/07-Evr_HobbyBook-Dictionaries/Unsolved/HobbyBook_Unsolved.py
@@ -1,1 +1,13 @@
 # @TODO: Your code here
+my_pet = {
+    "name": "Lucky",
+    "age": 10,
+    "hobbies": ["running", "barking", "sleeping"],
+    "wake_up": {"Monday": "7 am", "Tuesday": "8 am", "Wednesday": "9 am"
+    }
+}
+
+# We use an f-string to convert any object into a string
+    # Refer to the dictionary and then the individual key-value pair you would like to print
+print(f'Hello my name is {my_pet["name"]} and I am {my_pet["age"]}.') 
+

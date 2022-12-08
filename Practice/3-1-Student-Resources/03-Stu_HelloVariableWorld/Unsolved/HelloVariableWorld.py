96402f119bc3d3289b1f7f3b7b69cf86f0fed60b
Marco <marco_mireles@outlook.com>
2022-12-07 22:19:46 -0600

    practice

--- Practice/3-1-Student-Resources/03-Stu_HelloVariableWorld/Unsolved/HelloVariableWorld.py
+++ Practice/3-1-Student-Resources/03-Stu_HelloVariableWorld/Unsolved/HelloVariableWorld.py
@@ -9,22 +9,26 @@ country = "United States of America"
 age = 25
 
 # Create a variable called 'hourly_wage' that holds an integer
-
+hourly_wage = 15
 
 # Calculate the daily wage for the user
-
+daily_wage = hourly_wage * 8
 
 # Create a variable called 'satisfied' that holds a boolean
-
+satisfied = True
 
 # Print out "Hello <name>!"
-
+# "Hello" is a string that we want printed out
+# name is the variable
+# "!" is also a string 
+# Effectively you are building a formula to print a desired statement
+print("Hello" + name + "!")
 
 # Print out what country the user entered
-
+print("You live in" + country)
 
 # Print out the user's age
-
+print("You are" +str(age) + "years old")
 
 # With an f-string, print out the daily wage that was calculated
 

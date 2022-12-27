509340219e06713d00900dddc9817e2a65b2a9c2
Marco <marco_mireles@outlook.com>
2022-12-26 22:48:27 -0600

    notes

--- Practice/3-2-Student-Resources/05-Stu_NumberChain/Unsolved/NumberChain_Unsolved.py
+++ Practice/3-2-Student-Resources/05-Stu_NumberChain/Unsolved/NumberChain_Unsolved.py
@@ -5,12 +5,14 @@ user_play = "y"
 while user_play == "y":
 
     # Ask the user how many numbers to loop through
+    user_number = int(input('How many numbers?'))
     
     # Loop through the numbers. (Be sure to cast the string into an integer.)
-   
+    for x in range(user_number):
+        
 
         # Print each number in the range
-       
+        print(x)
 
     # Once complete...
     user_play = input("Continue: (y)es or (n)o? ")

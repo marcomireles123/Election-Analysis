acaa79d198d6e13bcc39897f22160f7844b4ccb8
Marco <marco_mireles@outlook.com>
2022-12-28 23:50:22 -0600

    notes

--- Python_practice.py
+++ Python_practice.py
@@ -1,4 +1,14 @@
 print("Hello World")
 
 # The type() command will tell you the data type of whatever is inside the parentheses
+    # This will print that 3 is an 'int' (integer) class 
 type(3)
+
+type(2019)
+
+# When typing #'s greater than 999 DO NOT use a comma 
+    # This will turn the object type into a 'Tuple'
+    # You will not get one integer in return, you end up with an ordered pair
+ballots = 1,341
+ballots
+type(ballots)

8d4b5a99c873836564a75b10f2fb34e8ada12464
Marco <marco_mireles@outlook.com>
2023-01-01 20:35:43 -0600

    order of precedence

--- Python_practice.py
+++ Python_practice.py
@@ -12,3 +12,48 @@ type(2019)
 ballots = 1,341
 ballots
 type(ballots)
+
+# Floating point decimals
+type(73.81)
+
+# Strings
+    # Can use single or double quotation marks
+    # Cannot be used for mathematical operations
+    # Even a number inside the quotation marks will be considered a string NOT an integer or floating point
+        # Data types can be converted to the right ones
+type('Hello world')
+
+# Empty strings
+    # Nothing in between the opening and closing quotation marks
+type('')
+
+# Boolean
+    # No quotation marks
+    # Can only be true or false
+type(True)
+
+# Creating variables
+    # Declared when followed up by an = sign
+    # Variables can contain integers, floating-point decimals, strings, and booleans
+    # Variable names must be in snake case (no space between words, instead an _ is used)
+    # Cannot use reserved words in Python for variable names
+        # Refer to this list with: help("keywords") command
+num_candidates = 3
+winning_percentage = 73.81
+candidate = 'Diane'
+won_election = True
+
+# 3.2.4 Skill Drill (1)
+5+2*3
+
+# The // divides one number by another and returns an integer
+    # AKA floor division
+8//5-3
+
+8+22*2-4
+
+16-3/2+7-1
+
+3**3%5
+
+5+9*3/2-4

ac3ce9cf4859be9ef9ae01da6816798dd15f1a1b
Marco <marco_mireles@outlook.com>
2023-01-03 00:20:28 -0600

    3.2.7 notes

--- Python_practice.py
+++ Python_practice.py
@@ -191,3 +191,53 @@ voting_data.insert(1, {'county': 'Jefferson', 'registered_voters': 432438})
 # Add “Arapahoe” and its registered voters to voting_data
 voting_data.append({"county":"Arapahoe", "registered_voters": 422829})
 voting_data
+
+# 3.2.8 Decision Statements
+
+# Practice using the if statement on our counties list to determine if the second county in the list is Denver. 
+    # If so, then we will print "Denver" to the screen
+counties = ["Arapahoe","Denver","Jefferson"]
+    # == is the sign for 'equal to'
+if counties[1] == 'Denver':
+    print(counties[1])
+
+# if-else statements
+temperature = int(input("What is the temperature outside? "))
+if temperature > 80:
+    print("Turn on the AC.")
+else:
+    print("Open the windows.")
+
+# Nested if-else statements
+#What is the score?
+score = int(input("What is your test score? "))
+# Determine the grade.
+if score >= 90:
+    print('Your grade is an A.')
+else:
+    if score >= 80:
+        print('Your grade is a B.')
+    else:
+        if score >= 70:
+            print('Your grade is a C.')
+        else:
+            if score >= 60:
+                print('Your grade is a D.')
+            else:
+                print('Your grade is an F.')
+
+# Alt code for if-else statements
+# What is the score?
+score = int(input("What is your test score? "))
+
+# Determine the grade.
+if score >= 90:
+    print('Your grade is an A.')
+elif score >= 80:
+    print('Your grade is a B.')
+elif score >= 70:
+    print('Your grade is a C.')
+elif score >= 60:
+    print('Your grade is a D.')
+else:
+    print('Your grade is an F.')

ae4e8e1b21b4265de7a626cb6ea374b694889ad8
Marco <marco_mireles@outlook.com>
2023-01-20 09:39:10 -0600

    3.5.3 notes

--- PyPoll.py
+++ PyPoll.py
@@ -18,28 +18,37 @@ file_to_save = os.path.join("analysis", "election_analysis.txt")
 # 1. Initialize a total vote counter.
 total_votes = 0
 
+# Candidate Options
+candidate_options = []
+
+# 1. Declare the empty dictionary.
+candidate_votes = {}
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
-     # To do: read and analyze the data here
-    # Read the file object with the reader function.
     file_reader = csv.reader(election_data)
 
-    # Read and print the header row.
+    # Read the header row.
     headers = next(file_reader)
 
     # Print each row in the CSV file.
     for row in file_reader:
-    # 2. Add to the total vote count.
+        # Add to the total vote count.
         total_votes += 1
 
-# 3. Print the total votes.
-print(total_votes)
+        # Print the candidate name from each row.
+        candidate_name = row[2]
 
+        if candidate_name not in candidate_options:
 
+           # Add the candidate name to the candidate list.
+            candidate_options.append(candidate_name)
 
+           # 2. Begin tracking that candidate's vote count.
+            candidate_votes[candidate_name] = 0
 
-# Using the with statement open the file as a text file.
-with open(file_to_save, "w") as txt_file:
+        # Add a vote to that candidate's count.
+        candidate_votes[candidate_name] += 1
 
-    # Write three counties to the file.
-     txt_file.write("Counties in the election\n-------------------------\nArapahoe\nDenver\nJefferson")
+# Print the candidate vote dictionary.
+print(candidate_votes)
